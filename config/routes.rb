7378496669d8cbf4619eafa255c16f79420ac998
takayasakaguchi <simple.pips@icloud.com>
2020-01-21 03:08:19 +0900

    ユーザー管理の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
+# ルーティングを追加
 Rails.application.routes.draw do
-  get 'messages/index'
-
-  root "messages#index"
+  devise_for :users
+  root 'messages#index'
+  resources :users, only: [:edit, :update]
 end

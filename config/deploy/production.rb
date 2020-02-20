61d9d31d3c185dc50627f32d6777ad253193c25d
takayasakaguchi <simple.pips@icloud.com>
2020-02-20 23:40:52 +0900

    capfile

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '18.180.25.44', user: 'ec2-user', roles: %w{app db web}
+
 
 # role-based syntax
 # ==================
@@ -59,3 +59,4 @@ server '18.180.25.44', user: 'ec2-user', roles: %w{app db web}
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+server '18.180.25.44', user: 'ec2-user', roles: %w{app db web}
